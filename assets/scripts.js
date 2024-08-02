9cf6d526e074bf92cd9101181ccc57dc05efd9a8
Saga-prod <damir.sagadbekov@gmail.com>
2024-08-02 07:00:58 +0200

    Version Finale

--- assets/scripts.js
+++ assets/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
             sm: 2,
             md: 3,
             lg: 3,
-            xl: 3
+            xl: 3,
         },
         lightBox: true,
         lightboxId: 'myAwesomeLightbox',
